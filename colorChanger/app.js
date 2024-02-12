da6c486bd9ab0e15a88528d17b935884bd4de3c3
Smrity Baral <baralsmrity8@gmail.com>
2024-02-12 20:51:47 +0530

    BMI calculater

--- colorChanger/app.js
+++ colorChanger/app.js
@@ -1,22 +1,22 @@
-const buttons = document.querySelectorAll(".button")
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
 
-const body = document.querySelector("body")
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
 
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please give a valid height ${height}`;
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please give a valid weight ${weight}`;
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        //show the result
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+});
 
-buttons.forEach(function (button) {
-    button.addEventListener('click', function (e) {
-        if (e.target.id === 'grey') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'pink') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'blue') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'yellow') {
-            body.style.backgroundColor = e.target.id;
-        }
-    })
-})
