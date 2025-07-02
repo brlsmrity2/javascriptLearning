17df07f4d266b4af6141df1b434afcd3be83d946
brlsmrity2 <baralsmrity8@gmail.com>
2025-07-02 17:26:47 +0530

    currency converter

---
+++ fetch API/script.js
@@ -0,0 +1,19 @@
+// let heading = document.getElementById("heading");
+// console.log(heading);
+
+
+// let classheading = document.getElementsByClassName("heading-class");
+// console.log(classheading);
+
+// let parahs = document.getElementsByTagName("p");
+// console.log(parahs)
+
+
+// let query = document.querySelector("p");
+// console.log(query.innerText)
+
+
+
+
+// let queries = document.querySelectorAll("p");
+// console.log(queries)
